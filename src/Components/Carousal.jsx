7a4afb588b9023f04414d2da026ea20beb405245
KARAN-SINGH19 <115944579+KARAN-SINGH19@users.noreply.github.com>
2024-06-04 04:23:44 +0400

    updaetd

--- src/Components/Carousal.jsx
+++ src/Components/Carousal.jsx
@@ -16,38 +16,38 @@ const Carousal = () => {
     };
 
     return (
-        <Carousel fade  activeIndex={activeIndex} onSelect={handleSelect}>
-            <Carousel.Item style={{ height: '550px' }} interval={2000}>
+        <Carousel fade activeIndex={activeIndex} onSelect={handleSelect}>
+            <Carousel.Item style={{ height: '600px' }} interval={4000}>
                 <img
-                    className="d-block w-100"
+                    className="d-block w-100 blurImage"
                     src="/images/bulb.jpg"
                     alt="Placeholder slide"
                     style={{ objectFit: 'cover', height: '100%' }}
                 />
                 <Carousel.Caption style={{ marginBottom: '110px', color: 'white', textShadow: '3px 3px 4px rgba(0, 0, 0, 0.9)', marginRight: "80px" }}>
-                    <motion.h4 style={{ marginTop: '20px', color: "white", textAlign: "left", fontSize: "40px", marginLeft: "-50px" }} variants={linkVariants} initial="hidden" animate={activeIndex === 0 ? "show" : "hidden"} >Enable individuals to thrive <br/>amidst today's challenges and <br/>seize tomorrow's opportunities <br/>in a dynamic, diverse economy.</motion.h4>
+                    <motion.h4 style={{ marginTop: '20px', color: "white", textAlign: "left", fontSize: "45px", marginLeft: "-50px", textShadow: '4px 3px 4px rgba(0, 0, 0, 0.9)' }} variants={linkVariants} initial="hidden" animate={activeIndex === 0 ? "show" : "hidden"} >Enable individuals to thrive <br />amidst today's challenges and <br />seize tomorrow's opportunities <br />in a dynamic, diverse economy.</motion.h4>
                 </Carousel.Caption>
             </Carousel.Item>
-            <Carousel.Item style={{ height: '550px' }} interval={2000}>
+            <Carousel.Item style={{ height: '600px' }} interval={4000}>
                 <img
-                    className="d-block w-100"
+                    className="d-block w-100 blurImage"
                     src="/images/bulb2.jpg"
                     alt="Placeholder slide"
                     style={{ objectFit: 'cover', height: '100%' }}
                 />
                 <Carousel.Caption style={{ marginBottom: '190px', color: 'white', textShadow: '3px 3px 4px rgba(0, 0, 0, 0.9)', marginRight: "20px" }}>
-                    <motion.h4 style={{ color: "white", textAlign: "left", fontSize: "40px", marginLeft: "-50px" }} variants={linkVariants} initial="hidden" animate={activeIndex === 1 ? "show" : "hidden"} >Empower and nurture local talent <br/> in the GCC Region.</motion.h4>
+                    <motion.h4 style={{ color: "white", textAlign: "center", fontSize: "45px", marginLeft: "-50px", textShadow: '2px 2px 4px rgba(0, 0, 0, 0.5)' }} variants={linkVariants} initial="hidden" animate={activeIndex === 1 ? "show" : "hidden"} >Empower and nurture local talent <br /> in the GCC Region.</motion.h4>
                 </Carousel.Caption>
             </Carousel.Item>
-            <Carousel.Item style={{ height: '550px' }} interval={2000}>
+            <Carousel.Item style={{ height: '600px' }} interval={4000}>
                 <img
-                    className="d-block w-100"
+                    className="d-block w-100 blurImage"
                     src="/images/bulb3.jpg"
                     alt="Placeholder slide"
                     style={{ objectFit: 'cover', height: '100%' }}
                 />
                 <Carousel.Caption style={{ marginBottom: '150px', color: 'white', textShadow: '3px 3px 4px rgba(0, 0, 0, 0.9)' }}>
-                    <motion.h4 style={{ color: "white", textAlign: "left", fontSize: "40px", marginRight: "20px", marginLeft: "-50px" }} variants={linkVariants} initial="hidden" animate={activeIndex === 2 ? "show" : "hidden"} >Elevate expertise and insights <br/> within  functional domains to <br/> drive business ambitions.</motion.h4>
+                    <motion.h4 style={{ color: "white", textAlign: "right", fontSize: "45px", marginRight: "20px", marginLeft: "-50px", textShadow: '2px 2px 4px rgba(0, 0, 0, 0.5)' }} variants={linkVariants} initial="hidden" animate={activeIndex === 2 ? "show" : "hidden"} >Elevate expertise and insights <br /> within  functional domains to <br /> drive business ambitions.</motion.h4>
                 </Carousel.Caption>
             </Carousel.Item>
         </Carousel>
