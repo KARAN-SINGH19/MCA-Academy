ef74279414e6a6ed351ed43453eb2ecdbdaa828c
KARAN-SINGH19 <115944579+KARAN-SINGH19@users.noreply.github.com>
2024-06-06 17:51:05 +0400

    code updaetd!

--- src/Screens/Home.jsx
+++ src/Screens/Home.jsx
@@ -9,6 +9,7 @@ import pillars from '../Api/Pillars';
 import { motion } from 'framer-motion';
 import reveal from '../Animations/Variants';
 import { useInView } from 'react-intersection-observer';
+import '@fortawesome/fontawesome-free/css/all.min.css';
 import Slider from '../Components/Slider';
 import academy from '../Api/Academy';
 import Container from 'react-bootstrap/Container';
@@ -95,75 +96,129 @@ const Home = () => {
     );
   }
 
-  const circles = [...Array(5)].map((_, i) => {
-    const angle = (i / 5) * 2 * Math.PI;
-    const radius = 120;
-    const x = 50 + (radius * Math.cos(angle)) / 70 * 50;
-    const y = 50 + (radius * Math.sin(angle)) / 70 * 50;
-    return { x, y };
-  });
-
-
 
   const RedBarSection = () => {
-    const handleClickMore = () => {
-      console.log("Clicked on Click More");
-      // Add your logic here
+    const values = [
+      { icon1: '/images/logo1.jpg', icon: 'fas fa-chart-line', text: 'Unlocking Authentic Leadership' },
+      { icon: 'fas fa-balance-scale', text: 'Entrepreneurial Excellence -  Skills and mindset' },
+      { icon: 'fas fa-award', text: 'Empowering Women Leaders For Success' },
+      { icon: 'fas fa-handshake', text: 'Fostering Functional Expertise' },
+      { icon: 'fas fa-users', text: 'Coaching for Professionals' },
+    ];
+
+    const angleStep = Math.PI / (values.length - 1);
+    const radius = 300;
+
+    const scrollToSection = (sectionId) => {
+      const section = document.getElementById(sectionId);
+      if (section) {
+        section.scrollIntoView({ behavior: "smooth" });
+      }
     };
 
+
     return (
-      <Container fluid style={{ paddingLeft: 0, paddingRight: 0, marginLeft: 0, marginInlineStart: 0, width: '100%' }}>
+      <Container fluid style={{
+        paddingLeft: 0, paddingRight: 0, overflow: 'hidden',
+      }}>
         <Row>
           <Col>
-            <div className="image-container2">
-           
-              
-                <div className="box2" style={{ marginTop: "50px" }}>
-                  <h2 style={{ color: 'white', marginTop: '25px' }}>Our Objective</h2>
-                  <p style={{ color: 'white', padding: '20px', fontSize: '20px' }}>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam ratione, dolores necessitatibus atque eum consequuntur voluptate quia architecto et, beatae dolorum sed dolor voluptas laborum quasi repellendus accusantium nobis praesentium?
-                  </p>
-                </div>
-                <div className="py-5 text-center position-relative circle-container" style={{ marginTop: "180px" }}>
+            <div style={{
+              textAlign: 'center',
+              padding: '50px',
+              position: 'relative',
+              height: "100vh",
+              backgroundImage: 'url(/images/bst.jpg)',
+              backgroundSize: 'cover',
+              backgroundPosition: 'center',
+              backgroundRepeat: 'no-repeat'
+            }}>
+              <div style={{
+                position: 'absolute',
+                top: 0,
+                left: 0,
+                width: '100%',
+                height: '100%',
+                backgroundColor: 'rgba(102, 3, 3, 0.75)',
+                zIndex: 0
+              }}></div>
 
-                <div className="central-circle2" onClick={handleClickMore} style={{ zIndex: 1 }}>
-                    Click More <br />
-                  </div>
-                  <div className="arrow-container">
-                    <div className="arrow-bounce">
-                      <div role="img" aria-label="down-arrow" style={{ animation: 'bounce 1s infinite' }}>👇</div>
+              <div style={{
+                position: 'relative',
+                zIndex: 1,
+                backgroundColor: 'rgba(0, 0, 0, 0.3)',
+                padding: '20px',
+                borderRadius: '10px',
+                boxShadow: '0 4px 8px rgba(0, 0, 0, 0.2)',
+                marginBottom: '40px'
+              }}>
+                <h1 style={{ color: 'white', marginBottom: '20px' }}>Academy Verticals</h1>
+                <p style={{ color: "white", fontSize: "20px", textAlign: "justify" }}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus officia corporis tenetur. Earum harum repellendus praesentium eius iste enim, cupiditate doloribus hic culpa placeat? Ab quaerat aut nemo.  Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus officia corporis tenetur. Earum harum repellendus praesentium eius iste enim, cupiditate doloribus hic culpa placeat? Ab quaerat aut nemo et animi!
+                </p>
+              </div>
+
+              {values.map((value, index) => {
+                const angle = index * angleStep - Math.PI;
+                const x = radius * Math.cos(angle);
+                const y = radius * Math.sin(angle);
+                return (
+                  <div
+                    key={index}
+                    style={{
+                      position: 'absolute',
+                      left: `calc(50% + ${x}px)`,
+                      top: `calc(90% + ${y}px)`,
+                      transform: 'translate(-50%, -50%)',
+                      textAlign: 'center',
+                      zIndex: 1,
+                    }}
+                  >
+                    <div
+                      style={{
+                        background: '#FF6D00',
+                        borderRadius: '50%',
+                        width: '120px',
+                        height: '120px',
+                        display: 'flex',
+                        alignItems: 'center',
+                        justifyContent: 'center',
+                        margin: '0 auto',
+                      }}
+                    >
+                      <i className={value.icon} style={{ fontSize: '50px', color: 'white' }}></i>
+                      {/* <img src={value.icon} style={{ width:"80px",height:"80px" }} alt="" /> */}
                     </div>
+                    <p style={{ color: 'white', maxWidth: '190px' }}>{value.text}</p>
                   </div>
-                  <div className="surrounding-circles">
-                    {/* Assume circles and pillars are defined elsewhere */}
-                    {circles.map((circle, i) => (
-                      <div
-                        key={i}
-                        className='surrounding-circle'
-                        style={{
-                          top: `${circle.y}%`,
-                          left: `${circle.x}%`,
-                          transform: 'translate(-50%, -50%)',
-                          textAlign: 'center',
-                          padding: '5px',
-                          fontSize: '0.85rem',
-                          textDecoration: 'none',
-                          transition: 'transform 0.3s ease',
-                          cursor: "pointer"
-                        }}
-                      >
-                        {pillars[i % pillars.length].title}
-                      </div>
-                    ))}
+                );
+              })}
+
+              <div style={{ position: 'absolute', display: 'inline-block', zIndex: 2 }}>
+                <div
+                  style={{
+                    background: '#FF6D00',
+                    borderRadius: '50%',
+                    width: '460px',
+                    height: '450px',
+                    position: 'absolute',
+                    bottom: '-75px',
+                    left: '50%',
+                    transform: 'translateX(-50%)',
+                    zIndex: 1,
+                    top: 'calc(100% - -300px)',
+                  }}
+                >
+                  <div>
+                    <h1 onClick={() => { scrollToSection('pillars2') }} style={{ color: "white", display: "flex", alignItems: "center", justifyContent: "center", marginTop: "70px", cursor: "pointer", animation: "bounce 1s infinite" }}>CLICK MORE</h1>
                   </div>
                 </div>
-            
+              </div>
             </div>
           </Col>
         </Row>
       </Container>
     );
-  }
+  };
 
 
   const PillarSection = () => {
@@ -177,9 +232,29 @@ const Home = () => {
       <Container fluid style={{ width: "100%" }}>
         <Row>
           <Col>
-            <div className="mission-section py-5 text-center bg-light" ref={ref}>
-              <h2 className="mission-heading">Academy Verticals</h2>
+            <div className="" ref={ref}>
               <RedBarSection />
+            </div>
+          </Col>
+        </Row>
+
+      </Container>
+    );
+  }
+
+
+  const PillarSection2 = () => {
+    const { ref, inView } = useInView({
+      triggerOnce: true,
+      threshold: 0,
+    });
+
+
+    return (
+      <Container fluid style={{ width: "100%" }}>
+        <Row>
+          <Col>
+            <div className="" ref={ref}>
               <PillarCard data={pillars} />
             </div>
           </Col>
@@ -221,13 +296,21 @@ const Home = () => {
         <MissionSection />
       </section>
 
-      {/* <CounterSection /> */}
-
       <section id="pillars">
         <Container fluid>
           <Row>
             <Col>
               <PillarSection />
+            </Col>
+          </Row>
+        </Container>
+      </section>
+      <br /> <br /> <br /> <br /> <br /> <br />
+      <section id="pillars2">
+        <Container fluid>
+          <Row>
+            <Col>
+              <PillarSection2 />
             </Col>
           </Row>
         </Container>
