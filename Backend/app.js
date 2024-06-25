7316df88cc38dc4895bf2ca98717abe0e36f4516
KARAN-SINGH19 <115944579+KARAN-SINGH19@users.noreply.github.com>
2024-06-25 23:20:23 +0400

    Update app.js

--- Backend/app.js
+++ Backend/app.js
@@ -3,12 +3,11 @@ const cors = require('cors');
 const app = express();
 const emailRoute = require('./emailRoute');
 
-// Allow requests from specific origin (replace with your frontend URL)
 const corsOptions = {
-  origin: 'https://mca-academy-frontend.vercel.app',
-  methods: ['POST', 'GET', 'OPTIONS'], // Allow the methods you need
-  allowedHeaders: ['Content-Type', 'Authorization'], // Add any other headers your app uses
-  credentials: false // Set to true if your frontend sends credentials (cookies, etc.)
+    origin: 'https://mca-academy-frontend.vercel.app',
+    methods: ['POST', 'GET', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: false // Set to true if your frontend sends credentials (cookies, etc.)
 };
 
 app.use(cors(corsOptions));
